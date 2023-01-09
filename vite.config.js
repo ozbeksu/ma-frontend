533bebcf709d08c924dd6aabad4f96b32892dbc7
Suleyman Ozbek <ozbek.su@gmail.com>
2023-01-09 16:13:52 +0300

    moved styles to scss

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {additionalData: `@import "./src/styles/all.scss";`},
+    },
+  },
 });
